624e5c909fc99541ba688d18b49aca1a6a0039df
Mohammed Abdul Raheem <mohdraheem68@gmail.com>
2022-02-27 21:48:49 +0530

    Added projects section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,28 @@
 let navBar = document.querySelector('#navigation');
 let menuBtn = document.querySelector('.hamburger');
 let mainSection = document.querySelector('.main-section');
-let isMenuOpen = true;
+let isMenuOpen = false;
 menuBtn.addEventListener('click', (event)=> {
     menuBtn.classList.toggle('open');
     if(isMenuOpen) {
         navBar.style.flex = 0;
+        navBar.style.minWidth = "0";
         isMenuOpen = false;
     } else {
         navBar.style.flex = 1;
+        navBar.style.minWidth = "250px";
         isMenuOpen = true;
     }
 })
 
+// Navigation click handler for mobile view
+let menuLinks = document.querySelectorAll('.nav-link');
+console.log({menuLinks})
+for (const link of menuLinks) {
+    link.addEventListener('click', (event) => {
+        menuBtn.classList.toggle('open');
+        navBar.style.flex = 0;
+        navBar.style.minWidth = "0";
+        isMenuOpen = false;
+    })
+}
